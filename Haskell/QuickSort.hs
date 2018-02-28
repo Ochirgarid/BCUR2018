0d1018b7cad83bdb3d5ceadc444d207e3a240c21
Tumurtogtokh Davaakhuu <tdavaakhuu1@sheffield.ac.uk>
2018-02-28 13:26:16 +0000

    added file reader and timer

--- Haskell/QuickSort.hs
+++ Haskell/QuickSort.hs
@@ -1,2 +1,8 @@
-sort :: [Int] - > [Int]
+module QuickSort where
 
+quickSort :: (Ord a) => [a] -> [a]  
+quickSort [] = []  
+quickSort (x:xs) =   
+    let smSorted = quickSort [a | a <- xs, a <= x]  
+        bgSorted = quickSort [a | a <- xs, a > x]  
+    in  smSorted ++ [x] ++ bgSorted
---
+++ Haskell/input.hs
@@ -0,0 +1,28 @@
+import Control.Exception
+import Data.Time
+import System.IO  
+import Control.Monad
+
+import QuickSort
+
+readF :: [String] -> [Int]
+readF = map read
+
+main = do  
+    let fin = []
+    handle <- openFile "../data.in" ReadMode
+    contents <- hGetContents handle
+    let singlewords = words contents
+        fin = readF singlewords
+    start <- getCurrentTime
+    evaluate (quickSort fin) -- Change sorting functions here
+    end <- getCurrentTime        
+
+    print "TestCase: "
+    print fin
+    print "Outpout: "    
+    print (quickSort fin)
+    print "Time: "    
+    print (diffUTCTime end start)    
+
+    hClose handle
