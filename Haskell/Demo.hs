f34b1a1b641e0ba3c7c8283309e367471b041c46
Tumurtogtokh Davaakhuu <tdavaakhuu1@sheffield.ac.uk>
2018-03-21 22:31:01 +0000

    20March

--- Haskell/Demo.hs
+++ Haskell/Demo.hs
@@ -1,23 +1,17 @@
--- module Benchmarking where
-import System.Environment
 import Control.Exception
 import System.IO
---import System.TimeIt
 
 -- Sorting modules
 import QuickSort
-
--- main 
--- main :: IO()
+main :: IO()
 main = do
     let fin = []
     handle <- openFile "../data.in" ReadMode
     contents <- hGetContents handle
     let singlewords = words contents
         fin = readF singlewords
-    
-    -- using TimeIt
-    --timeIt $ evaluate (quickSort fin) -- Change sorting functions here    
+
+    evaluate (quickSort fin) -- Change sorting functions here    
     
     hClose handle
     
