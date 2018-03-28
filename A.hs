00d8e0d4e36124456b5b7e467978af8ae6580899
Tumurtogtokh Davaakhuu <tdavaakhuu1@sheffield.ac.uk>
2018-03-28 13:44:13 +0100

    cleared trash files

--- A.hs
+++
@@ -1,9 +0,0 @@
-import System.Environment
-import Text.Printf
-
-main = do
-    [d] <- map read `fmap` getArgs
-    printf "%f\n" (mean [1..d])
-
-mean :: [Double] -> Double
-mean xs = sum xs / fromIntegral (length xs)
